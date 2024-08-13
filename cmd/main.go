1eb80ca6d783f58db8e47d455154a4d0fba467a8
FrancoRutigliano <francorutigliano11@gmail.com>
2024-08-12 23:54:09 -0300

    main initial config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"taskTracker/internal/cli"
+)
+
 func main() {
 
+	if len(os.Args) < 2 {
+		log.Fatalf("Please try again, you have to provide two arguments, and have %v", len(os.Args))
+	}
+	fmt.Println("What do you want to do?")
+	cli.HandleCommand(os.Args[1:])
+	fmt.Println("Goodbye...")
 }
