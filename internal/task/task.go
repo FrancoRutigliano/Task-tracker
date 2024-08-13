512a09738673e099479e7b8346c735071029e22a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-08-12 23:55:03 -0300

    define the struct of the task

---
+++ internal/task/task.go
@@ -0,0 +1,17 @@
+package task
+
+import "time"
+
+type Task struct {
+	Id          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Tasks []Task
+
+func (t *Tasks) NewTask(description string) string {
+	return description
+}
